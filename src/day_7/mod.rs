c95f4c0b2e2db9250ea3549431ed8c5df9f915d9
PhazonicRidley <ma13hew@gmail.com>
2022-12-29 21:51:26 -0800

    completed day 7 part 1

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -17,17 +17,15 @@ fn folder_content_size(contents: &Vec<String>) -> i32 {
         if let Ok(size) = entry_split[0].parse::<i32>() {
             sums.push(size);
         }
-        if entry.contains("..") {
-            multiplier += 1;
-        }
+        // if entry.contains("..") {
+        //     multiplier += 1;
+        // }
     }
 
-    // if multiplier > 1 {
-    //     multiplier =- 1;
-    // }
+    
     println!("multiplier: {}", multiplier);
 
-    return (sums.iter().sum::<i32>()) * if multiplier != 0 { multiplier } else { 1 };
+    return (sums.iter().sum::<i32>()); // * if multiplier != 0 { multiplier } else { 1 };
 }
 
 fn part_one(puzzle_input: &Vec<String>) -> i32 {
@@ -40,6 +38,7 @@ fn part_one(puzzle_input: &Vec<String>) -> i32 {
     command_indices.reverse();
     let mut current_directory_path: Vec<&str> = vec![];
     let mut dir_sums: HashMap<String, i32> = HashMap::new();
+    let mut counted_dirs: Vec<String> = vec![];
     let mut command_idx = 0;
     let mut cd_str = String::new();
     let max_cmd_index = command_indices[0];
@@ -52,14 +51,16 @@ fn part_one(puzzle_input: &Vec<String>) -> i32 {
                 cd_str = current_directory_path.join("/").to_string();
                 dir_sums.insert(cd_str.clone(), 0);
             } else {
-                let mut total_dir_sum = dir_sums.get(&cd_str).unwrap().to_owned();
-                let sub_dir_sums: i32 = dir_sums
-                    .iter()
-                    .filter(|(k, _)| k.contains(&(cd_str.clone() + "/")))
-                    .map(|(_, &v)| v)
-                    .sum();
+                let mut total_dir_sum: i32 = dir_sums.get(&cd_str).unwrap().to_owned();
+                
+                for (dir_str, size) in &dir_sums {
+                    if dir_str.contains(&(cd_str.clone() + "/")) && !counted_dirs.contains(dir_str) {
+                        counted_dirs.push(dir_str.clone());
+                        total_dir_sum += size;
+                    }
+                    
+                }
 
-                total_dir_sum += sub_dir_sums;
                 dir_sums.insert(cd_str.clone(), total_dir_sum);
                 current_directory_path.remove(current_directory_path.len() - 1);
                 cd_str = current_directory_path.join("/").to_string();
@@ -73,10 +74,10 @@ fn part_one(puzzle_input: &Vec<String>) -> i32 {
             command_idx = next_idx;
         }
 
-        command_indices.drain_filter(|n| n.to_owned() <= command_idx);
+        command_indices.drain_filter(|&mut n| n <= command_idx);
     }
 
-    dir_sums.insert(String::from("/"), dir_sums.values().sum());
+    
     let mut counter = 0;
     let mut final_sum = 0;
     for value in dir_sums.values() {
@@ -85,9 +86,7 @@ fn part_one(puzzle_input: &Vec<String>) -> i32 {
             final_sum += value;
         }
     }
-    println!("Final Dir Sums: {:?}", dir_sums);
-    println!("Number under 100000: {}", counter);
-    println!("Length: {:?}", dir_sums.len());
+
     return final_sum;
 }
 
